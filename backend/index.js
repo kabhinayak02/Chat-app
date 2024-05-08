a976d087cbbe5993e59bc0690ed615e86a2fc3f4
Abhinay Kumar <kabhinayak02@gmail.com>
2024-05-08 20:48:30 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,26 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+import userRoutes from "./routes/user.route.js"
+
 import connectToMongoDB from "./db/connectToMongoDB.js"
-
-
-const app = express();
+import {app, server} from "./socket/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 8000
 
-app.use("/api/auth", authRoutes)
+const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello world!!");
-    
-})
+app.use(express.json());  // to parse the incoming requests with JSON payloads (from req.body) 
+app.use(cookieParser());
 
-app.listen(PORT, ()=>{
-    connectToMongoDB()
-    console.log(`server is listning at port ${PORT}`)
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+server.listen(PORT, ()=>{
+    connectToMongoDB();
+    console.log(`server is listning at port ${PORT}`);
+}) 
